f2845c925ef2bd1eda3c87c470468152e3fa43d8
melhemm <jamalmulhemm@gmail.com>
2022-01-05 21:26:32 +0300

    account id and profile image added

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,18 @@
 class AccountsController < ApplicationController
   before_action :authenticate_account!
+  before_action :set_account, only: [:profile]
 
   def index
     @posts = Post.active
   end
 
-  def show; end
+  def profile
+    @posts = @account.posts.active
+  end
+
+  private
+
+  def set_account
+    @account = Account.find_by_username(params[:username])
+  end
 end
