6aca157baaaead0a60e21bc8c7596661ea639dd2
melhemm <jamalmulhemm@gmail.com>
2022-01-05 21:27:46 +0300

    follower and following numbers added

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include ImageUploader::Attachment(:image)
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,5 +8,13 @@ class Account < ApplicationRecord
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def total_followers
+    0
+  end
+
+  def total_following
+    0
   end
 end
