ff00d8f6da4a9284ac889fea34f75ba624de23ee
melhemm <jamalmulhemm@gmail.com>
2022-03-20 02:25:55 +0300

    follow function added

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,9 @@ class AccountsController < ApplicationController
 
   def index
     @posts = Post.active
-    @follower_suggestions = Account.where.not(id: current_account.id)
+    following_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
+    following_ids <<  current_account.id
+    @follower_suggestions = Account.where.not(id: following_ids)
   end
 
   def profile
@@ -12,8 +14,14 @@ class AccountsController < ApplicationController
   end
 
   def follow_account
-    account_id = params[:follow_id]
-    
+    follower_id = params[:follow_id]
+    if Follower.create!(follower_id: current_account.id, following_id: follower_id)
+      flash[:success] = "You follow this account" 
+    else
+      flash[:danger] = "Unable to follow this account" 
+    end
+
+    redirect_to dashboard_path 
   end
 
   private
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,6 +1,6 @@
 module AccountsHelper
-  def profile_picture account, width = 100
-    image_path = account.image.present? ? account.image.thumb.url : "default.png"
+  def profile_picture account, width = 300
+    image_path = account.image.present? ? account.image.url : "default.png"
     image_tag(image_path, width: width ,class: "img-circle")
   end
 
